a95b9d42c14f9eddbb5a749765e064e1267f0aef
benjoquilario <benjoquilario@gmail.com>
2023-09-14 16:19:00 +0800

    add swMinify and set to true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require("next-contentlayer")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
