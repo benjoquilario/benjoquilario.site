948490daea2ff5ec7e03bcd0fd2377df739a2dd6
benjoquilario <benjoquilario@gmail.com>
2024-09-27 20:07:39 +0800

    feat: add more uses

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -10,7 +10,27 @@ const Uses = () => {
       </TypographyH3>
 
       <div>
-        <div>Desk</div>
+        <div className="font-semibold">Desk</div>
+        <ul className="my-6 ml-6 list-disc md:text-[15px] [&>li]:mt-2">
+          <li>Hp Dell Laptop Intel Core I7</li>
+        </ul>
+
+        <div className="font-semibold">Development</div>
+        <ul className="my-6 ml-6 list-disc md:text-[15px] [&>li]:mt-2">
+          <li>VSCode</li>
+          <li>
+            One Dark Pro -{" "}
+            <span className="text-foreground/70">Theme for VSCode</span>
+          </li>
+          <li>
+            Github Copilot - <span className="text-foreground/70">Copilot</span>
+          </li>
+          <li>Github Desktop</li>
+
+          <li>
+            Vercel - <span className="text-foreground/70">Hosting</span>
+          </li>
+        </ul>
       </div>
     </section>
   )
